7c2fba1ddc3ece601a7388dccf4d4d4dc64a2e98
dontsellshort <michaelhly@gmail.com>
2018-12-14 04:08:43 -0600

    added bidding functions

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -31,7 +31,8 @@ contract RegistryVars {
         bytes32 cdp;
         address buyer;
         uint256 value;
-        bytes32 bidID;
+        address token;
+        bytes32 bidId;
         uint256 expiryBlockTimestamp;
     }
 }
@@ -46,17 +47,17 @@ contract AuctionHouse is Pausable, Ownable, RegistryVars{
         mkr = IMakerCDP(_mkrAddress);
     }
 
-    // Mapping of auctionIDs to its corresponding CDP auction
+    // Mapping of auctionIds to its corresponding CDP auction
     mapping (bytes32 => AuctionInfo) internal auctions;
     // Mapping for iterative lookup of all auctions
     mapping (uint256 => AuctionInfo) internal allAuctions;
    
-    // Mapping of auctionIDs to max bidID
-    mapping (bytes32 => bytes32) public maxBid;
-    // Mapping of auctionIDs to bids
-    mapping (bytes32 => uint256[]) public auctionToBids;
-    // Registry mapping bidIDs to their corresponding entries
+    // Registry mapping bidIds to their corresponding entries
     mapping (bytes32 => BidInfo) internal bidRegistry;
+    // Mapping of auctionIds to bidIds
+    mapping (bytes32 => bytes32[]) internal auctionToBids;
+    // Mapping of revoked bids
+    mapping (bytes32 => bool) public revokedBids;
 
     event LogAuctionEntry(
         bytes32 cdp,
@@ -72,7 +73,7 @@ contract AuctionHouse is Pausable, Ownable, RegistryVars{
         bytes32 indexed auctionId
     );
 
-    function getAuction(bytes32 auctionID)
+    function getAuction(bytes32 auctionId)
         public
         view
         returns (
@@ -83,11 +84,11 @@ contract AuctionHouse is Pausable, Ownable, RegistryVars{
             AuctionState state
         )
     {
-        number = auctions[auctionID].listingNumber;
-        seller = auctions[auctionID].seller;
-        token = auctions[auctionID].token;
-        expiry = auctions[auctionID].expiryBlockTimestamp;
-        state = auctions[auctionID].state;
+        number = auctions[auctionId].listingNumber;
+        seller = auctions[auctionId].seller;
+        token = auctions[auctionId].token;
+        expiry = auctions[auctionId].expiryBlockTimestamp;
+        state = auctions[auctionId].state;
     }
 
     function getAuctionByIndex(uint256 index)
@@ -111,79 +112,145 @@ contract AuctionHouse is Pausable, Ownable, RegistryVars{
 
     /* List a CDP for auction */
     function listCDP(
-        bytes32 _cdp,
-        address _token,
-        uint256 _expiry,
-        uint _salt
+        bytes32 cdp,
+        address token,
+        uint256 expiry,
+        uint salt
     )
         external
         whenNotPaused
         returns (bytes32)
     {
-        require(msg.sender == mkr.lad(_cdp), "currently does not support CDP proxies");
+        require(msg.sender == mkr.lad(cdp), "currently no support for CDP proxies");
 
-        bytes32 auctionID = _genAuctionId(
+        bytes32 auctionId = _genAuctionId(
             ++totalListings,
-            _cdp,
+            cdp,
             msg.sender,
-            _token,
-            _expiry,
-            _salt 
+            expiry,
+            salt
         );
 
-        require(auctions[auctionID].state == AuctionState.Undefined);
+        require(auctions[auctionId].state == AuctionState.Undefined);
 
          AuctionInfo memory entry = AuctionInfo(
             totalListings,
-            _cdp, 
+            cdp,
             msg.sender,
-            _token,
-            _expiry,
+            token,
+            expiry,
             AuctionState.WaitingForBids
         );
 
-        auctions[auctionID] = entry;
+        auctions[auctionId] = entry;
         allAuctions[totalListings] = entry;
 
         emit LogAuctionEntry(
-            _cdp,
+            cdp,
             msg.sender,
-            auctionID,
-            _token,
-            _expiry
+            auctionId,
+            token,
+            expiry
         );
 
-        return auctionID;
+        return auctionId;
     }
 
     /* Remove a CDP from auction */
-    function cancelAuction(bytes32 auctionID)
+    function cancelAuction(bytes32 auctionId)
         external
     {
-        AuctionInfo memory entry = auctions[auctionID];
+        AuctionInfo memory entry = auctions[auctionId];
         require(entry.state == AuctionState.WaitingForBids);
         require(msg.sender == entry.seller);
 
         entry.state = AuctionState.Cancelled;
-        auctions[auctionID] = entry;
+        auctions[auctionId] = entry;
 
         emit LogCancelledAuction(
             entry.cdp,
             entry.seller,
-            auctionID
+            auctionId
         );
     }
 
+    /* Submit a bid to auction */
+    function submitBid(
+        bytes32 auctionId,
+        uint256 value,
+        uint256 expiry,
+        uint salt
+    )
+        external
+        whenNotPaused
+        returns (bytes32)
+    {
+        AuctionInfo memory entry = auctions[auctionId];
+
+        require(entry.seller != msg.sender);
+
+        if(entry.expiryBlockTimestamp > block.timestamp) {
+            entry.state = AuctionState.Expired;
+            auctions[auctionId] = entry;
+            allAuctions[entry.listingNumber] = entry;
+            return bytes32(0);
+        }
+
+        require(
+            entry.state == AuctionState.WaitingForBids ||
+            entry.state == AuctionState.Live
+        );
+        require(IERC20(entry.token).transferFrom(msg.sender, this, value));
+
+        if(entry.state == AuctionState.WaitingForBids) {
+            entry.state = AuctionState.Live;
+            auctions[auctionId] = entry;
+            allAuctions[entry.listingNumber] = entry;
+        }
+
+        bytes32 bidId = _genBidId(
+            auctionId,
+            msg.sender,
+            value,
+            expiry,
+            salt
+        );
+
+        BidInfo memory bid = BidInfo(
+            entry.cdp,
+            msg.sender,
+            value,
+            entry.token,
+            bidId,
+            expiry
+        );
+
+        bidRegistry[bidId] = bid;
+        auctionToBids[auctionId].push(bidId);
+
+        return bidId;
+    }
+
+    /* RevokeBid from auction */
+    function revokeBid(bytes32 bidId)
+        external
+    {
+        BidInfo memory bid = bidRegistry[bidId];
+        require(msg.sender == bid.buyer);
+        require(!revokedBids[bidId]);
+        revokedBids[bidId] = true;
+        require(IERC20(bid.token).transfer(msg.sender, bid.value));
+    }
+
     /**
-     * Helper function for computing the hash of a given listing, 
-     * which will be used as the auctionID for each new CDP 
+     * Helper function for computing the hash of a given auction
+     * listing. Will be used as the auctionId for each new CDP
      * auctions. 
      */
     function _genAuctionId(
         uint256 _auctionCounter,
         bytes32 _cup, 
         address _seller, 
-        address _token,
         uint256 _expiry, 
         uint _salt
     )
@@ -196,7 +263,32 @@ contract AuctionHouse is Pausable, Ownable, RegistryVars{
                 _auctionCounter,
                 _cup, 
                 _seller, 
-                _token,
+                _expiry,
+                _salt
+            )
+        );
+    }
+
+    /**
+     * Helper function for computing the hash of a given bid.
+     * Will be used as the bidId for each bid in an auction.
+     */
+    function _genBidId(
+        bytes32 _auctionId,
+        address _buyer,
+        uint256 _value,
+        uint256 _expiry,
+        uint _salt
+    )
+        internal
+        pure
+        returns(bytes32)
+    {
+        return keccak256(
+            abi.encodePacked(
+                _auctionId,
+                _buyer,
+                _value,
                 _expiry,
                 _salt
             )
