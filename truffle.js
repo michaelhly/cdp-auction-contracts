343a91b64a3db1c7176f7b3090315ae1106885ce
dontsellshort <michaelhly@gmail.com>
2018-12-16 19:07:50 -0600

    deployment stuff

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -15,4 +15,23 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    kovan: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 42,
+      from: "0x4e82303b75dc96c02a2eb3765d016187bd5bec14",
+      gas: 4700000,
+      gasPrice: 30000000000
+    }
+  },
+  optimizer: {
+    enabled: true,
+    runs: 500
+  }
 };
