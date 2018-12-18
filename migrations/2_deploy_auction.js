3e57ebcbf2af0cc0915cbc897ea8c5b4516cabdc
dontsellshort <michaelhly@gmail.com>
2018-12-18 13:40:42 -0600

    reployed to Kovan

--- migrations/2_deploy_auction.js
+++ migrations/2_deploy_auction.js
@@ -1,10 +1,6 @@
-const DSProxyCache = artifacts.require("DSProxyCache");
-const DSProxy = artifacts.require("DSProxy");
 const Auction = artifacts.require("Auction");
-const MKR = "0xa71937147b55deb8a530c7229c442fd3f31b7db2";
+const SaiTub = "0xa71937147b55deb8a530c7229c442fd3f31b7db2";
 
 module.exports = function(deployer) {
-  deployer.deploy(DSProxyCache).then(function() {
-    return deployer.deploy(Auction, MKR, DSProxyCache.address);
-  });
+  deployer.deploy(Auction, SaiTub);
 };
