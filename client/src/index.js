6c7282ed04176758c12e98be207bc7fdb98851ae
dontsellshort <michaelhly@gmail.com>
2018-12-20 17:48:52 -0600

    return index.js to original

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-function AppWrapper() {
-  return (
-    <Web3Provider>
-      <App />
-    </Web3Provider>
-  );
-}
-
-ReactDOM.render(<AppWrapper />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
